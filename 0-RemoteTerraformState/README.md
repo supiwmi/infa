eddb8bfa9e6f4bc479be426c34eb44eac03fec7e
supiwmi <supiwmi@gmail.com>
2020-11-30 19:45:04 +0700

    id

--- 0-RemoteTerraformState/README.md
+++
@@ -1,8 +0,0 @@
-# Keep Terraform State at Local
-ถ้าต้องการเก็บ terraform stater ไว้ที่ local ก็ไม่ต้องรัน terraform init, terraform apply ใน 0-RemoteTerraformStateฃ
-
-# Keep Terraform State in AWS S3 Bucket
-แต่ถ้าเรามีเรามี s3 bucket อยู่แล้วก็เอาไปไว้ที่ remote ก็ได้เพื่อที่ว่าเราจะได้ไปรัน share script นี้ไปให้คืนอื่นรันหรือเราเองไปรันที่เครื่องไหนก็ได้ (แต่ต้องติดตั้ง AWS CLI และ configure aws user profile ให้เรียบร้อยก่อน)
-
-# Keep Terraform State in Terraform Cloud (Free)
-ถ้าไม่มี aws account เราสามารถใช้ terraform cloud เพื่อ เก็บ state ไฟล์ก็ได้ ฟรี แต่ต้อง register และ set configuration ให้เรียบร้อยก่อนเช่นกัน
--- 1-CreateResourceGroup/rg.tf
+++ 1-CreateResourceGroup/rg.tf
@@ -8,20 +8,8 @@
 # Configure the Azure Provider
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  #version = "=1.38.0"
-  version = "=2.20.0"
+  version = "=1.38.0"
 
-# subscription_id = "69c3d8de-1812-4e7b-a1ea-d4bdf2727ce9"    #<--- Suparuek
-# subscription_id = "28e7413d-4da6-4281-a940-af6c67d5cc14"    #<--- Supawat
-# subscription_id = "7450d6a3-c62a-4b03-a328-9212a2827341"    #<--- Supawan
-# subscription_id = "94061c49-560e-4893-8742-1bc78226b26a"    #<--- Varaporn
-# subscription_id = "a87466a6-ec2a-47b8-b830-112715fa6c4d"    #<--- TPITDevOps
-# subscription_id = "51adbb53-59e7-4448-824a-7fdea4254423"    #<--- Krit
-  
-  subscription_id = "69c3d8de-1812-4e7b-a1ea-d4bdf2727ce9"    #<--- Suparuek
-  tenant_id       = "4d73b592-41a0-4c4c-b446-283c89d92545"
-
-  features {}
 }
  
 # Create a resource group - RG-TF-VM-Identity
